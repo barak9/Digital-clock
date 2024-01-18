af33d0a159f493f5c0f3375ad889a06b052e5b0c
unknown <pkparveenbarak@gmail.com>
2024-01-18 18:19:24 +0530

    first commit

---
+++ time.js
@@ -0,0 +1,14 @@
+let hrs=document.getElementById("hrs")
+let mins= document.getElementById("mins")
+let sec= document.getElementById("sec")
+setInterval(()=>{
+
+
+let CurrentTime= new Date();
+//console.log(CurrentTime.getMinutes());
+hrs.innerHTML=CurrentTime.getHours();
+mins.innerHTML=CurrentTime.getMinutes();
+sec.innerHTML=CurrentTime.getSeconds();
+
+
+},1000)
